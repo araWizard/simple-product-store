dd2c125586fa51a4dabc5ae031b89bd5cda9330f
araWizard <acharya.aditya@outlook.com>
2025-03-11 04:33:43 +0000

    README Credit + Deployment Ready

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Simple-Product-Store
 A simple responsive MERN app which allows you to list products for sale. You can add, delete and modify the listed products
+
+## Acknowledgement
+This repository is based on the YouTube tutorial :
+[MERN Stack Tutorial with Deployment – Beginner's Course][https://www.youtube.com/watch?v=O3BUHwfHf84] by freeCodeCamp
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 import productRoutes from "./routes/product.route.js";
 
@@ -11,9 +12,19 @@ dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept in req.body (middleware function)
 
 app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
